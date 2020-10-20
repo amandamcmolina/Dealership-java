89690af0404c3b31cc0b0069d8760edc2e56e67e
amandamcmolina <56768735+amandamcmolina@users.noreply.github.com>
2020-10-20 15:52:20 -0300

    sisteminha

---
+++ src/com/dealership/entity/employee/ToSell.java
@@ -0,0 +1,10 @@
+package com.dealership.entity.employee;
+
+public interface ToSell {
+    //Quem contratar pode vender
+    // Ação de vender
+    // Dependendo do usuário, precisa de autorização
+
+    abstract boolean toSell();
+
+}
